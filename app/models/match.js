7d69c14c2d4a2032aa511723a2665f1e91a0eb4d
Guillermo Eijo <guillermo.eijo@fing.edu.uy>
2017-08-31 19:27:55 -0300

    Add match model

---
+++ app/models/match.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from 'mongoose';
+
+var matchSchema = new Schema ({
+    id : { 
+        type: Number,
+        unique: true
+    },
+    isRealTime : Boolean,
+    players : Array,
+    owner : String,
+    endingDate : Date,
+    game : {
+        id : { type: Number, index: true },
+        name : String,
+        description : String,
+        rating : Number,
+        timesPlayed : Number,
+        creator : String,
+        questions : Array,
+        tags : [String],
+        ranking : [(String,Number)],
+        creationDate : Date,
+        image : String
+    },
+    result : Array
+})
+
+class MatchClass {}
+
+matchSchema.loadClass(MatchClass);
+
+export default mongoose.model('Match', matchSchema);
