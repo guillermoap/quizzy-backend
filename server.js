a05728d109062b36cd2d5e65e3d96b8a6f2836a5
Mauricio Irace <thelagger94@gmail.com>
2017-08-31 18:33:27 -0300

    se agregaron las rutas al server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import users from './config/routes/users.js';
 import configuration from './config/config.js';
+import games from './config/routes/games';
 
 var env = process.env.NODE_ENV || 'development';
 var config = configuration[env];
@@ -23,6 +24,7 @@ if ((env != 'test') && (env != 'prodTest')) {
 
 //Middleware to set the general routing
 app.use('/users', users);
+app.use('/games',games);
 
 const server = app.listen(config.server.port, config.server.host, () => {
   const { address, port } = server.address();
