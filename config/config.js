89a386a6ce37d616558971f17c45e5694500c0ef
guillermo.aguirre <guillermoaguirre1@gmail.com>
2017-09-02 15:49:29 -0300

    Add prodTest config, change travis.yml

--- config/config.js
+++ config/config.js
@@ -10,26 +10,17 @@ var configuration = {
       port: '3000'
     }
   },
-<<<<<<< a05728d109062b36cd2d5e65e3d96b8a6f2836a5
   test: {
     database: {
       host: '127.0.0.1',
       port: '27017',
       db: 'quizzy-backend-test'
-=======
-  production: {
-    database: {
-      host: '127.0.0.1',
-      port: '27017',
-      db: 'quizzy-backend'
->>>>>>> Add users specs, add config file
     },
     server: {
       host: 'localhost',
       port: '3000'
     }
   },
-<<<<<<< a05728d109062b36cd2d5e65e3d96b8a6f2836a5
   production: {
     database: {
       host: '172.31.29.19',
@@ -38,16 +29,6 @@ var configuration = {
     },
     server: {
       host: '172.31.25.108',
-=======
-  test: {
-    database: {
-      host: '127.0.0.1',
-      port: '27017',
-      db: 'quizzy-backend-test'
-    },
-    server: {
-      host: 'localhost',
->>>>>>> Add users specs, add config file
       port: '3000'
     }
   }
