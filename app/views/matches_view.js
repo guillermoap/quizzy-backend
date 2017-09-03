354bd484ccd98d5b278adacc0e5f4d3dc801fad2
Guillermo Eijo <guillermoeijo@gmail.com>
2017-09-03 18:31:22 -0300

    Add match factory and  basic test functionality

--- app/views/matches_view.js
+++ app/views/matches_view.js
@@ -1,19 +1,34 @@
 function matchIndex(matches) {
-    return {
-        matches: matches.map((match) => matchShow(match))  
-    }
+  return {
+    matches: matches.map(match => ({
+      id: match._id,
+      url: match.url,
+      isRealTime: match.isRealTime,
+      players: match.players,
+      owner: match.owner,
+      endingDate: match.endingDate,
+      game: match.game,
+      result: match.result
+    }))
+  }
 }
-  
+
 function matchShow(match) {
-    return {
-        id: match._id,
-        isRealTime: match.isRealTime,
-        players: match.players,
-        owner: match.owner,
-        endingDate: match.endingDate,
-        game: match.game,
-        result: match.result
+  return {
+    match: {
+      id: match._id,
+      url: match.url,
+      isRealTime: match.isRealTime,
+      players: match.players,
+      owner: match.owner,
+      endingDate: match.endingDate,
+      game: match.game,
+      result: match.result
     }
+  }
 }
-    
-export { matchIndex, matchShow }
+
+export {
+  matchIndex,
+  matchShow
+}
--- spec/factories/factory.js
+++ spec/factories/factory.js
@@ -2,6 +2,7 @@ import factory from 'factory-girl';
 import faker from 'faker';
 import User from '../../app/models/user';
 import Game from '../../app/models/game';
+import Match from '../../app/models/match';
 
 factory.define('user', User, {
     email: () => faker.internet.email(),
@@ -30,6 +31,55 @@ factory.define('game', Game, {
     "ranking": [],
     "creationDate": () => faker.date.past(),
     "image": "fake base64"
+});
+
+factory.define('match', Match, {
+    isRealTime: () => faker.random.boolean(),
+    players: ['player1', 'player2', 'player3'],
+    owner: () => faker.fake('{{name.firstName}} {{name.lastName}}'),
+    endingDate: () => faker.date.future(),
+    game: {
+        // id: { type: () => faker.random.number },
+        name: () => faker.fake('{{name.firstName}} {{name.lastName}}'),
+        description: () => faker.lorem.sentence(),
+        rating: () => faker.random.number(),
+        timesPlayed: () => faker.random.number(),
+        creator: () => faker.fake('{{name.firstName}} {{name.lastName}}'),
+        questions: [
+            {
+                // id: { type: () => faker.random.number() },
+                text: () => faker.lorem.sentence(),
+                difficulty: () => faker.random.arrayElement(["low", "medium", "high"]),
+                answers: ['resp1', 'resp2', 'resp3', 'resp4'],
+                correctAnswer: () => faker.random.number({ min:1, max:4 })
+            },
+            {
+                text: () => faker.lorem.sentence(),
+                difficulty: () => faker.random.arrayElement(["low", "medium", "high"]),
+                answers: ['resp1', 'resp2', 'resp3', 'resp4'],
+                correctAnswer: () => faker.random.number({ min:1, max:4 })
+            },
+            {
+                text: () => faker.lorem.sentence(),
+                difficulty: () => faker.random.arrayElement(["low", "medium", "high"]),
+                answers: ['resp1', 'resp2', 'resp3', 'resp4'],
+                correctAnswer: () => faker.random.number({ min:1, max:4 })
+            },
+            {
+                text: () => faker.lorem.sentence(),
+                difficulty: () => faker.random.arrayElement(["low", "medium", "high"]),
+                answers: ['resp1', 'resp2', 'resp3', 'resp4'],
+                correctAnswer: () => faker.random.number({ min:1, max:4 })
+            }],
+        tags: ['tag1', 'tag2', 'tag3'],
+        ranking: [{
+            user: () => faker.internet.userName(),
+            points: () => faker.random.number()
+        }],
+        endingDate: () => faker.date.past(),
+        image: () => faker.image.imageUrl()
+    },
+    result: [1, 2, 3]
 });
 
 export default factory;
