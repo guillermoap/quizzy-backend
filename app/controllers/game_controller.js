e971ba1e3b2cac5c51c32edadf06704a4dd0c943
Bruno Gonzalez <bruno.prp@gmail.com>
2017-09-04 21:42:29 -0300

    Fix indentation

--- app/controllers/game_controller.js
+++ app/controllers/game_controller.js
@@ -1,11 +1,16 @@
 import Game from '../models/game';
-import { gameIndex, gameShow } from '../views/games_view';
+import {
+  gameIndex,
+  gameShow
+} from '../views/games_view';
 
 export const index = (req, res, next) => {
   Game.find().lean().exec((err, games) => {
     if (err) {
       return res.status(400)
-      .json({ error: err.errmsg });
+        .json({
+          error: err.errmsg
+        });
     } else {
       res.json(gameIndex(games));
     }
@@ -16,7 +21,9 @@ export const show = (req, res, next) => {
   Game.findById(req.params.id).lean().exec((err, game) => {
     if (err) {
       return res.status(400)
-      .json({ error: err.errmsg });
+        .json({
+          error: err.errmsg
+        });
     } else {
       res.json(gameShow(game));
     }
@@ -24,21 +31,29 @@ export const show = (req, res, next) => {
 };
 
 export const create = (req, res, next) => {
-  Game.create(req.body.game, function (err, game) {
+  Game.create(req.body.game, function(err, game) {
     if (err) {
       return res.status(400)
-      .json({ error: err.errmsg });
+        .json({
+          error: err.errmsg
+        });
     } else {
       return res.json({});
     }
   });
 };
 
-export const update = (req, res, next)  => {
-  Game.findByIdAndUpdate(req.params.id, { $set: req.body.game }, { new: true }).exec((err, game) => {
+export const update = (req, res, next) => {
+  Game.findByIdAndUpdate(req.params.id, {
+    $set: req.body.game
+  }, {
+    new: true
+  }).exec((err, game) => {
     if (err) {
       return res.status(400)
-      .json({ error: err.errmsg });
+        .json({
+          error: err.errmsg
+        });
     } else {
       return res.json(gameShow(game));
     }
@@ -49,7 +64,9 @@ export const destroy = (req, res, next) => {
   Game.findByIdAndRemove(req.params.id).exec((err, game) => {
     if (err) {
       return res.status(400)
-      .json({ error: err.errmsg });
+        .json({
+          error: err.errmsg
+        });
     } else {
       return res.json({});
     }
