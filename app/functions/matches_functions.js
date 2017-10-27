0973546ac3a18199c2feb7b8b58b0de8dc094820
diego alberti <diegoalberti8@hotmail.com>
2017-10-26 23:24:52 -0300

    delete comments and debug

--- app/functions/matches_functions.js
+++ app/functions/matches_functions.js
@@ -1,25 +1,20 @@
 function rankingInsert(match, user, points) {
-  let ranking = match.game.ranking.slice();  //copia el ranking del juego en una variable ranking
-  let userPosition = ranking.findIndex(greaterOrEqual);    //busca en que posición va a quedar el user en el ranking
+  let ranking = match.game.ranking.slice();
+  let userPosition = ranking.findIndex(greaterOrEqual);
   if (userPosition === -1) {
     //add user at the end of the ranking
     userPosition = ranking.size;
   }
-  ranking.splice(userPosition, 0, {   //la función splice agrega al ranking en la posición userPosition el elemento. El 0 va así
+  ranking.splice(userPosition, 0, {
     user: user,
     points: points
   });
-
-   //para terminar, sobreescribir el ranking del game del match con la variable ranking, y devolver esa variable como resultado de la funcion       para utilizar en el fronten
   match.game.ranking = ranking;
-  console.log('   Tiki chupala')
   return match;
   function greaterOrEqual(item) {
     return points >= item.points;
   }
 };
-
-
 
 export {
   rankingInsert
